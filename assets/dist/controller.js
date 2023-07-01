c8a6760e2a531ca82e4996a5e08727eec31d0477
matthieu <rei_eva@hotmail.com>
2023-07-01 20:48:07 +0200

    add emoji field

--- assets/dist/controller.js
+++ assets/dist/controller.js
@@ -7,8 +7,12 @@ exports.default = void 0;
 var _stimulus = require("@hotwired/stimulus");
 var _quill = _interopRequireDefault(require("quill/dist/quill"));
 var _quillImageUploader = _interopRequireDefault(require("quill-image-uploader"));
-require("quill-image-uploader/dist/quill.imageUploader.min.css");
 var _axios = _interopRequireDefault(require("axios"));
+require("quill-image-uploader/dist/quill.imageUploader.min.css");
+require("quill-emoji/dist/quill-emoji.css");
+var Emoji = _interopRequireWildcard(require("quill-emoji"));
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -24,7 +28,8 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-_quill.default.register("modules/imageUploader", _quillImageUploader.default);
+_quill.default.register('modules/imageUploader', _quillImageUploader.default);
+_quill.default.register("modules/emoji", Emoji);
 var _default = /*#__PURE__*/function (_Controller) {
   _inherits(_default, _Controller);
   var _super = _createSuper(_default);
@@ -40,7 +45,9 @@ var _default = /*#__PURE__*/function (_Controller) {
       var options = {
         debug: this.extraOptionsValue.debug,
         modules: {
-          toolbar: toolbarOptionsValue
+          toolbar: toolbarOptionsValue,
+          "emoji-toolbar": true,
+          "emoji-shortname": true
         },
         placeholder: this.extraOptionsValue.placeholder,
         theme: this.extraOptionsValue.theme
@@ -51,11 +58,12 @@ var _default = /*#__PURE__*/function (_Controller) {
             upload: function upload(file) {
               return new Promise(function (resolve, reject) {
                 var formData = new FormData();
-                formData.append("file", file);
+                formData.append('file', file);
                 _axios.default.post(_this.extraOptionsValue.upload_handler.path, formData).then(function (response) {
                   resolve(response.data);
                 }).catch(function (err) {
-                  reject("Upload failed");
+                  reject('Upload failed');
+                  console.log(err);
                 });
               });
             }
@@ -68,7 +76,7 @@ var _default = /*#__PURE__*/function (_Controller) {
             upload: function upload(file) {
               return new Promise(function (resolve, reject) {
                 var reader = function reader(file) {
-                  return new Promise(function (resolve, reject) {
+                  return new Promise(function (resolve) {
                     var fileReader = new FileReader();
                     fileReader.onload = function () {
                       return resolve(fileReader.result);
@@ -84,7 +92,8 @@ var _default = /*#__PURE__*/function (_Controller) {
                   }).then(function (response) {
                     resolve(response.data);
                   }).catch(function (err) {
-                    reject("Upload failed");
+                    reject('Upload failed');
+                    console.log(err);
                   });
                 });
               });
@@ -97,7 +106,7 @@ var _default = /*#__PURE__*/function (_Controller) {
         this.editorContainerTarget.style.height = this.extraOptionsValue.height;
       }
       var quill = new _quill.default(this.editorContainerTarget, options);
-      quill.on('text-change', function (delta, deltaResult, source) {
+      quill.on('text-change', function () {
         var quillContent = quill.root.innerHTML;
         var inputContent = _this.inputTarget;
         inputContent.value = quillContent;
