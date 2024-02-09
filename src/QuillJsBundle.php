c2fb22bdaae62708520b0c499d2d075b05f069e4
Ehyiah <rei_eva@hotmail.com>
2024-02-09 18:08:07 +0100

    fix php cs fixer config

--- src/QuillJsBundle.php
+++ src/QuillJsBundle.php
@@ -5,4 +5,6 @@ namespace Ehyiah\QuillJsBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 // More details on https://symfony.com/doc/current/bundles/configuration.html#using-the-abstractbundle-class
-class QuillJsBundle extends Bundle {}
+class QuillJsBundle extends Bundle
+{
+}
