9a9fe63d3a63acf57cb15dfaaaba2a054296474b
matthieu <rei_eva@hotmail.com>
2024-06-12 15:25:50 +0200

    add highlight module

--- src/DTO/Options/Modules/SyntaxModule.php
+++ src/DTO/Options/Modules/SyntaxModule.php
@@ -5,4 +5,10 @@ namespace Ehyiah\QuillJsBundle\DTO\Options\Modules;
 class SyntaxModule
 {
     public const NAME = 'syntax';
+
+    public function __construct(
+        public string $name = self::NAME,
+        public string $options = 'true',
+    ) {
+    }
 }
