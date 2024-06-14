0c87ed14dd3bd6d37437f5ee32d03b8c775b063a
Ehyiah <rei_eva@hotmail.com>
2024-06-14 20:22:06 +0200

    add missing build files for javascript

---
+++ assets/dist/modules.js
@@ -0,0 +1,12 @@
+function mergeModules(modulesOptions, enabledModules) {
+  let modules = [];
+  modules = modulesOptions.reduce((acc, moduleOption) => {
+    acc[moduleOption.name] = moduleOption.options;
+    return acc;
+  }, {});
+  return {
+    ...modules,
+    ...enabledModules
+  };
+}
+export default mergeModules;
