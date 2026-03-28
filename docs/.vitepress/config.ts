25bd14edaed77c5b6352d22511f0b01868d5ea92
Ehyiah <rei_eva@hotmail.com>
2026-03-28 12:14:18 +0100

    regorganize doc menu for modules

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -37,25 +37,40 @@ export default defineConfig({
           },
           {
             text: 'Modules',
-            collapsed: true,
+            collapsed: false,
             items: [
               { text: 'Concept', link: '/guide/modules/index' },
-              { text: 'Autosave', link: '/guide/modules/autosave' },
-              { text: 'Image Selection', link: '/guide/modules/image-selection' },
-              { text: 'Markdown', link: '/guide/modules/markdown' },
-              { text: 'Mention', link: '/guide/modules/mention' },
-              { text: 'ReadTime', link: '/guide/modules/read-time' },
-              { text: 'Speech To Text', link: '/guide/modules/stt' },
-              { text: 'NodeMover', link: '/guide/modules/node-mover' },
-              { text: 'FullScreen', link: '/guide/modules/fullscreen' },
-              { text: 'Link Attributes', link: '/guide/modules/link-attributes' },
-              { text: 'Counter', link: '/guide/modules/counter' },
-              { text: 'Divider', link: '/guide/modules/divider' },
-              { text: 'HtmlEdit', link: '/guide/modules/html-edit' },
-              { text: 'PageBreak', link: '/guide/modules/pagebreak' },
-              { text: 'Paste Sanitizer', link: '/guide/modules/paste-sanitizer' },
-              { text: 'SmartLinks', link: '/guide/modules/smart-links' },
-              { text: 'Other Modules', link: '/guide/modules/others' }
+              {
+                text: 'Independent',
+                collapsed: true,
+                items: [
+                  { text: 'Autosave', link: '/guide/modules/autosave' },
+                  { text: 'Counter', link: '/guide/modules/counter' },
+                  { text: 'FullScreen', link: '/guide/modules/fullscreen' },
+                  { text: 'HtmlEdit', link: '/guide/modules/html-edit' },
+                  { text: 'Markdown', link: '/guide/modules/markdown' },
+                  { text: 'Mention', link: '/guide/modules/mention' },
+                  { text: 'NodeMover', link: '/guide/modules/node-mover' },
+                  { text: 'Paste Sanitizer', link: '/guide/modules/paste-sanitizer' },
+                  { text: 'ReadTime', link: '/guide/modules/read-time' },
+                  { text: 'SmartLinks', link: '/guide/modules/smart-links' },
+                  { text: 'Speech To Text', link: '/guide/modules/stt' },
+                  { text: 'Other Modules', link: '/guide/modules/others' }
+                ]
+              },
+              {
+                text: 'Field-dependent',
+                collapsed: true,
+                items: [
+                  { text: 'Divider', link: '/guide/modules/divider' },
+                  { text: 'Emoji', link: 'https://github.com/contentco/quill-emoji' },
+                  { text: 'Image Selection', link: '/guide/modules/image-selection' },
+                  { text: 'Link Attributes', link: '/guide/modules/link-attributes' },
+                  { text: 'PageBreak', link: '/guide/modules/pagebreak' },
+                  { text: 'Syntax (Highlight)', link: 'https://quilljs.com/docs/modules/syntax/' },
+                  { text: 'Table', link: 'https://github.com/attoae/quill-table-better' }
+                ]
+              },
             ]
           },
           { text: 'Advanced',
